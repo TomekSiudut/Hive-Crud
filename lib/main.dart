4789cbe3a398a88a5eaadf2ee5a459c90b7c78f4
tomek.siudut <tomasz.siudut@giraffestudio.pl>
2022-01-11 21:18:27 +0100

    hive init added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:hive_crud/screens/contact_screen.dart';
+import "package:path_provider/path_provider.dart" as path_provider;
 
-void main() {
+void main() async {
+  final appDocumentDirectory = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
   runApp(const MyApp());
 }
 
